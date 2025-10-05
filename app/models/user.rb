1746f0246afc57c4aea0d6f7d69bfde3136ffbf7
robson3999 <glodrbt@gmail.com>
2025-10-05 15:42:56 +0200

    [be] Disable user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable,
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   has_many :albums
   has_many :photos
 end
